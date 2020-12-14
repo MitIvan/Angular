5aee4ce1ea5077ea340ec3274c171deb7d6b3aa7
Ivan Mitev <ivan.mitev90@gmail.com>
2020-12-14 15:31:29 +0100

    update

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing= new Subject<number>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apple', 2),
@@ -13,6 +14,10 @@ export class ShoppingListService {
         return[...this.ingredients]
     }
 
+    getIngredient(index: number) {
+        return this.ingredients[index]
+    }
+
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient)
         this.ingredientsChanged.next([...this.ingredients])
@@ -21,5 +26,15 @@ export class ShoppingListService {
     addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients)
         this.ingredientsChanged.next([...this.ingredients])
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient
+        this.ingredientsChanged.next([...this.ingredients])
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1)
+        this.ingredientsChanged.next([...this.ingredients])   
     }
 }
